0bbe5fde7c9cbc89eaf96dbc0eb180eb917b2a3e
Baron221 <baronjon080@gmail.com>
2023-09-06 18:33:34 +0900

    fix:schema model modification

--- schema/member.model.js
+++ schema/member.model.js
@@ -1,57 +1,88 @@
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 const { member_status_enums, member_type_enums } = require("../lib/config");
-
+const { Timestamp } = require("mongodb");
 
 const memberSchema = new mongoose.Schema({
-    mb_nick:{
-        type:String,
-        required:true,
-        index:{unique:true , sparse:true}
+  mb_nick: {
+    type: String,
+    required: true,
+    index: { unique: true, sparse: true },
+  },
+  mb_phone: {
+    type: String,
+    required: true,
+  },
+  mb_password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  mb_type: {
+    type: String,
+    required: false,
+    default: "USER",
+    enum: {
+      values: member_type_enums,
+      message: "{VALUE} is not among permitted values",
     },
-    mb_phone:{
-        type:String,
-        required:true
+  },
+  mb_status: {
+    type: String,
+    required: false,
+    default: "ACTIVE",
+    enum: {
+      values: member_status_enums,
+      message: "{VALUE} is not among permitted values",
     },
-    mb_password:{
-        type:String,
-        required:true,
-        select:false
-    },
-    mb_type:{
-        type:String,
-        required:false,
-        default:"USER",
-        enum:{
-            values:member_type_enums,
-            message:"{VALUE} is not among permitted values"
-        }
-    },
-    mb_status:{
-        type:String,
-        required:false,
-        default:"ACTIVE",
-        enum:{
-            values:member_status_enums,
-            message:"{VALUE} is not among permitted values"
-        }
-    },
-    mb_full_name:{
-        type:String,
-        required:false
-    },
-    mb_address:{
-        type:String,
-        required:false
-    },
-    mb_image:{
-        type:String,
-        required:false
-    },
-    mb_point:{
-        type:Number,
-        required:false,
-        default:0
-    }
-});
+  },
 
-module.exports = mongoose.model("Member" , memberSchema)
+  mb_address: {
+    type: String,
+    required: false,
+  },
+  mb_description: {
+    type: String,
+    required: false,
+  },
+  mb_image: {
+    type: String,
+    required: false,
+  },
+  mb_point: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  mb_top: {
+    type: String,
+    required: false,
+    default: "N",
+    enum: {
+      values: ordernary_enums,
+      message: "{VALUE} is not among permitted values",
+    },
+  },
+  mb_views: {
+    type: Number,
+    reuired: false,
+    default: 0,
+  },
+  mb_likes: {
+    type: Number,
+    reuired: false,
+    default: 0,
+  },
+  mb_follow_cnt: {
+    type: Number,
+    reuired: false,
+    default: 0,
+  },
+  mb_subscriber_cnt: {
+    type: Number,
+    reuired: false,
+    default: 0,
+  },
+},
+{timestamps:true});
+
+module.exports = mongoose.model("Member", memberSchema);
