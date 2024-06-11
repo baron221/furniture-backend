98b0b58d7bba32eef1b41a57160188a68e7a406a
Baron221 <baronjon080@gmail.com>
2024-06-11 13:59:37 +0900

    feat:build socket.io server business logic

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const router = require("./router.js");
 const router_bssr = require("./router_bssr.js");
-const cors = require('cors');
-
+const cors = require("cors");
+const http = require("http");
 
 let session = require("express-session");
 const cookieParser = require("cookie-parser");
@@ -16,13 +16,15 @@ const store = new MongoDbStore({
 
 // 1 express ga kirib kelyatgan malumotlarga bogliq bolgan kodlar yoziladi. KIRISH kodlari
 app.use(express.static("public"));
-app.use("/uploads", express.static(__dirname + "/uploads"))
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(express.json()); // kirib kelyatgan json formatdagi data ni objectga ogiradi
 app.use(express.urlencoded({ extended: true })); //html forumdan request qiladi
-app.use(cors({
-  credentials:true,
-  origin:true,
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 app.use(cookieParser());
 //2 Session
 app.use(
@@ -49,4 +51,33 @@ app.set("view engine", "ejs");
 app.use("/furnis", router_bssr);
 app.use("/", router);
 
-module.exports = app;
+const server = http.createServer(app);
+/**SOCKET.IO backend  */
+const io = require("socket.io")(server, {
+  serveClient: false,
+  origins: "*:*",
+  transport: ["websocket", "xhr-polling"],
+});
+let online_users = 0;
+
+io.on("connection", function (socket) {
+  online_users++;
+  console.log("New User , total:", online_users);
+  socket.emit("greetMsg", { text: "Welcome" });
+  // socket.brodcast.emit();
+  io.emit("infoMsg", { total: online_users });
+
+  socket.on("disconnect", function () {
+    online_users--;
+    socket.broadcast.emit("infoMsg", { total: online_users });
+    console.log("client disconnected, total:", online_users);
+  });
+
+  socket.on('createMsg', function(data){
+    console.log(data);
+    io.emit('newMsg',data);
+  })
+});
+/**SOCKET.IO backend  */
+
+module.exports = server;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
 const http =require("http");
+
 const mongoose =require("mongoose");
 mongoose.set('strictQuery', true)
 
@@ -18,8 +18,7 @@ mongoose.connect(connectionString,
     else{
         console.log("MongoDB connection succeed");
         // console.log(goose)
-        const app =require("./app");
-        const server = http.createServer(app);
+        const server =require("./app");
         let PORT = process.env.PORT||3001;
         server.listen(PORT , function(){
             console.log(`The server is running succesfully on port : ${PORT} , http://localhost:${PORT}`)
