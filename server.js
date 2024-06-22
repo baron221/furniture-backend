9ca5c5d1756da05b393173e9f82ad42d25f4c0c7
Baron221 <baronjon080@gmail.com>
2024-06-22 23:25:46 +0900

    fix:server.js updated

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
-mongoose.set('strictQuery', false)
 
 const mongoose =require("mongoose");
 
 let db
 const connectionString = process.env.MONGO_URL
 
+mongoose.set('strictQuery', false)
 mongoose.connect(connectionString,
     {
      useNewUrlParser:true, 
